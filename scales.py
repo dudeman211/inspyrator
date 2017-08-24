05bc8f0192181738542fde1718d6c9d6a90deb1d
Sal Bruno <boozecode.reload@gmail.com>
2017-08-23 20:18:12 -0700

    Create scales.py

---
+++ scales.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+"""scales.py: Builds the different scale modes from the 12-note chromatic scale."""
+
+__version__ "0.01a"
+
+__author__ = "Sal Bruno"
+__copyright__ = "Copyright 2017, Sal Bruno"
+__license__ = "MIT"
+__status__ = "Prototype"
+
+# The 12 note chromatic scale.
+chromatic = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
