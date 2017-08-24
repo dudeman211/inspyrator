42c90b22b99a8c79fb4aa21ddb29d7f1783f1df4
Sal Bruno <boozecode.reload@gmail.com>
2017-08-24 07:22:22 -0700

    Update scales.py

--- scales.py
+++ scales.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-"""scales.py: Builds the different scale modes from the 12-note chromatic scale."""
+"""
+Scales.py: Builds the different scale modes from the 12-note chromatic scale.
+"""
 
-__version__ = "0.01a"
 
 __author__ = "Sal Bruno"
 __copyright__ = "Copyright 2017, Sal Bruno"
 __license__ = "MIT"
+__version__ = "0.01a"
 __status__ = "Prototype"
 
 # The 12 note chromatic scale.
@@ -15,6 +18,7 @@ chromatic = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
 # Scale Modes
 ionian = [2, 2, 1, 2, 2, 2, 1]  # The "Major" scale.
 
+
 def make_new_chrom(scale_key, a_chrom):
     """
     Rearranges the notes to start on given element.
@@ -22,4 +26,10 @@ def make_new_chrom(scale_key, a_chrom):
 
     new_chrom = a_chrom[scale_key:] + a_chrom[:scale_key]
     new_chrom.append(a_chrom[scale_key])  # Append first note to end of list.
+
     return new_chrom
+
+key_to_use = input("In which key would you like to work in? >").upper()
+new_chromatic = make_new_chrom(chromatic.index(key_to_use), chromatic)
+
+print(new_chromatic)
