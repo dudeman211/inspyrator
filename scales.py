28f3a1853c109a721738c263e2a152793863bb92
Sal Bruno <boozecode.reload@gmail.com>
2017-08-26 06:19:24 -0700

    Update scales.py

--- scales.py
+++ scales.py
@@ -15,8 +15,32 @@ __status__ = "Prototype"
 # The 12 note chromatic scale.
 chromatic = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
 
-# Scale Modes
-ionian = [2, 2, 1, 2, 2, 2, 1]  # The "Major" scale.
+# Base scale mode
+base_mode = [2, 2, 1, 2, 2, 2, 1]  # The "Major" scale.
+
+# Seven modes of the Major scale.
+modes = [
+    'ionian (Major)',
+    'dorian',
+    'phrygian',
+    'lydian',
+    'mixolydian',
+    'aeolian (minor)',
+    'locrian'
+    ]
+
+
+def is_valid_key(user_key, chrom):
+    """
+    Checks for valid user input.
+    """
+
+    if(len(user_key) != 1 and len(user_key) != 2):
+        return False
+    elif(user_key not in chrom):
+        return False
+    else:
+        return user_key
 
 
 def make_new_chrom(scale_key, chrom):
@@ -29,7 +53,14 @@ def make_new_chrom(scale_key, chrom):
 
     return new_chrom
 
-key_to_use = input("In which key would you like to work in? >").upper()
-new_chromatic = make_new_chrom(chromatic.index(key_to_use), chromatic)
 
-print(new_chromatic)
+key_to_use = input("In which key would you like to work in? >").upper()
+
+while(not is_valid_key(key_to_use, chromatic)):
+    print("I'm sorry, that is not valid input.")
+    key_to_use = input("In which key would you like to work in? >").upper()
+
+if(is_valid_key(key_to_use, chromatic)):
+    new_chromatic = make_new_chrom(chromatic.index(key_to_use), chromatic)
+
+    print(new_chromatic)
