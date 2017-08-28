c1fce58c0182edacdb0b8ca25c69cf7499c7ee34
Sal Bruno <boozecode.reload@gmail.com>
2017-08-27 17:22:05 -0700

    Update inspyrator.py

--- inspyrator.py
+++ inspyrator.py
@@ -16,7 +16,6 @@ while not build.is_valid_key(user_key, build.chromatic):
     print("I'm sorry, that is not valid input.")
     user_key = input("In which key would you like to work with?").upper()
 
-
 if build.is_valid_key(user_key, build.chromatic):
     new_chromatic = build.make_new_chrom(build.chromatic.index(user_key), build.chromatic)
     for step, mode_name in list(enumerate(build.modes)):
